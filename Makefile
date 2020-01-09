d6e8a0e5553e354582549b6ee10e8cab5340d7d5
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2020-01-09 16:41:01 +0000

    package setup
    Remove setup.py test function in favour of requirements-dev and make

---
+++ Makefile
@@ -0,0 +1,25 @@
+SHELL := /bin/bash
+VIRTUALENV_ROOT := $(shell [ -z $$VIRTUAL_ENV ] && echo $$(pwd)/venv || echo $$VIRTUAL_ENV)
+
+.PHONY: virtualenv
+virtualenv:
+	[ -z $$VIRTUAL_ENV ] && [ ! -d venv ] && python3 -m venv venv || true
+
+.PHONY: requirements
+requirements: virtualenv
+	${VIRTUALENV_ROOT}/bin/pip install -e .
+
+.PHONY: requirements-dev
+requirements-dev: virtualenv requirements-dev.txt
+	${VIRTUALENV_ROOT}/bin/pip install -r requirements-dev.txt
+
+.PHONY: test
+test: test-flake8 test-unit
+
+.PHONY: test-flake8
+test-flake8: virtualenv requirements-dev
+	${VIRTUALENV_ROOT}/bin/flake8 .
+
+.PHONY: test-unit
+test-unit: virtualenv requirements-dev
+	${VIRTUALENV_ROOT}/bin/py.test ${PYTEST_ARGS}
---
+++ setup.py
@@ -0,0 +1,29 @@
+"""
+A python client for D&B Direct+ API.
+"""
+import re
+import ast
+from setuptools import setup, find_packages
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('direct_plus_python_client/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    name='direct-plus-python-client',
+    version=version,
+    url='https://github.com/alphagov/direct-plus-python-client',
+    license='MIT',
+    author='GDS Developers',
+    description='A python client for D&B Direct+ API.',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests>=2.22.0,<3',
+    ],
+    python_requires='~=3.6',
+)
